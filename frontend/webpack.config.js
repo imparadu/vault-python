d66e5fbd08bb484402672ba8b08d72825fc07daf
imparadu <imparadu@gmail.com>
2025-05-13 14:58:03 +0300

    https eliminated

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,12 +1,12 @@
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 
 module.exports = {
   devServer: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'certs', 'private.key')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'certs', 'certificate.crt')),
-    },
+    // https: {
+    //   key: fs.readFileSync(path.resolve(__dirname, 'certs', 'private.key')),
+    //   cert: fs.readFileSync(path.resolve(__dirname, 'certs', 'certificate.crt')),
+    // },
     host: 'localhost',
     port: 3000,
   },
